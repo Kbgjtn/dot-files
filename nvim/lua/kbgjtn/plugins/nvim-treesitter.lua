e956f417428c06c7ee7e2b00fbe475cfa37595c5
Kbgjtn <kambingjan31@gmail.com>
2024-08-12 22:35:26 +0700

    feat(nvim): add pkl nvim

--- nvim/lua/kbgjtn/plugins/nvim-treesitter.lua
+++ nvim/lua/kbgjtn/plugins/nvim-treesitter.lua
@@ -62,4 +62,19 @@ return {
          }
       end,
    },
+   {
+      "https://github.com/apple/pkl-neovim",
+      lazy = true,
+      event = {
+         "BufReadPre *.pkl",
+         "BufReadPre *.pcf",
+         "BufReadPre PklProject",
+      },
+      dependencies = {
+         "nvim-treesitter/nvim-treesitter",
+      },
+      build = function()
+         vim.cmd("TSInstall! pkl")
+      end,
+   },
 }
