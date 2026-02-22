aac25aee3bb7873b4ff4b88255afc4aa654c9078
Kbgjtn <kambingjan31@gmail.com>
2026-02-22 22:45:10 +0000

    feat: add preference linux setup

---
+++ preferences_setup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# disabled screen-capture sound
+sudo mv /usr/share/sounds/freedesktop/stereo/screen-capture.oga /usr/share/sounds/freedesktop/stereo/screen-capture.oga.disabled
